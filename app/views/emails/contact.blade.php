929d504b85962fdac62e516740430b3990efbc48
Nathon Shultz <nathons@systemsedgeonline.com>
2014-05-30 22:36:18 -0500

    update

---
+++ app/views/emails/contact.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Legacy Realty Contact Us Form Submission</title>
+</head>
+<body>
+	<h2>Contact Us Form Submission</h2>
+	<p><strong>Name:</strong><br>{{$inputs['fullname']}}</p>
+	<br>
+	<p><strong>Email:</strong><br>{{$inputs['email']}}</p>
+	<br>
+	<p><strong>Subject:</strong><br>{{$inputs['msubject']}}</p>
+	<br>
+	<p><strong>Message:</strong><br>{{$inputs['mcontent']}}</p>
+</body>
+</html>
