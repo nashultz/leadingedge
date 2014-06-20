6fd16abe1f4c18962eb52879257c664183b0097b
Nathon Shultz <nathons@systemsedgeonline.com>
2014-06-20 11:59:01 -0500

    updated idx

--- app/views/idx/idx.blade.php
+++ app/views/idx/idx.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.sitedefault')
 
 @section('content')
-	<iframe src="http://matrix.abor.com/Matrix/public/IDX.aspx?idx=1cf1225" width="1024px" height="720px" frameborder="0" marginwidth="0" marginheight="0"></iframe>
+	<iframe src="http://david-mccaleb.idx.rewidx.com/" width="100%" height="2000" scrolling="yes" frameborder="0"></iframe>
 @stop
