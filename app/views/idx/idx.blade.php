060a403f86b9e8a6de74a4d5304f0304753b8b6f
SeoRoman <roman.lopez@roosterteeth.com>
2014-06-25 13:11:01 -0500

    Modified IDX Url

--- app/views/idx/idx.blade.php
+++ app/views/idx/idx.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.sitedefault')
 
 @section('content')
-	<iframe src="http://david-mccaleb.idx.rewidx.com?search_zip=78759&minimum_price=1000000&search_city=Austin,Bartlett" width="100%" height="2500" scrolling="no" frameborder="0"></iframe>
+	<iframe src="http://david-mccaleb.idx.rewidx.com" width="100%" height="2500" scrolling="no" frameborder="0"></iframe>
+	<!--
+		<iframe src="http://david-mccaleb.idx.rewidx.com?search_zip=78759&minimum_price=1000000&search_city=Austin,Bartlett" width="100%" height="2500" scrolling="no" frameborder="0"></iframe>
+	-->
 @stop
