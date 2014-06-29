b66afa2dc053cb0e0d6ece94268785a70bbdb33e
Nathon Shultz <nathons@systemsedgeonline.com>
2014-06-29 12:31:16 -0500

    updated favicon

--- app/views/layouts/sitedefault.blade.php
+++ app/views/layouts/sitedefault.blade.php
@@ -4,6 +4,8 @@
 	<title>Leading Edge Realty</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="favicon.ico" type="image/x-icon" />
 	
     <!-- Styles -->
     {{css('site','vendor/normalize.css')}}
